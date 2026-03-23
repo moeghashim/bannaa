55fa6b2c5d87f4432b3250827b34ec74c569d6a4
Ja3ood <moeghashim@users.noreply.github.com>
2026-03-23 13:08:37 -0500

    Add Google tag to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { IBM_Plex_Sans_Arabic, Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import type { PropsWithChildren } from "react";
 
 import "@/app/globals.css";
@@ -19,10 +20,27 @@ const arabic = IBM_Plex_Sans_Arabic({
   variable: "--font-arabic"
 });
 
+const GOOGLE_TAG_ID = "G-56QKJXZJNK";
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html className={`${inter.variable} ${grotesk.variable} ${arabic.variable}`} lang="en">
-      <body>{children}</body>
+      <body>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_TAG_ID}');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
